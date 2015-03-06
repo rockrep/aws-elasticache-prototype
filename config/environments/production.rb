7a38c4cfdf5f1e995e38e97c303366448e628564
rockrep <michael.kintzer@gmail.com>
2015-03-06 08:11:51 -0800

    try again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,11 @@ Rails.application.configure do
   config.cache_store = [ :dalli_store, elasticache.servers,
                          namespace: 'elasticache-proto', compress: true, expires_in: 3.hours ]
 
-  self.read_endpoints = ['fod-dev-redis-001.q8x6u8.0001.usw2.cache.amazonaws.com:6379',
+  self.redis_read_endpoints = ['fod-dev-redis-001.q8x6u8.0001.usw2.cache.amazonaws.com:6379',
                          'fod-dev-redis-002.q8x6u8.0001.usw2.cache.amazonaws.com:6379']
 
   self.redis_write = Redis.new host: 'fod-dev-redis.q8x6u8.ng.0001.usw2.cache.amazonaws.com', port: '6379', db: 0, logger: Rails.logger
-  self.redis_read  = Redis.new host: self.read_endpoints.first.split(':')[0], port: self.read_endpoints.first.split(':')[1], db: 0, logger: Rails.logger
+  self.redis_read  = Redis.new host: self.redis_read_endpoints.first.split(':')[0], port: self.redis_read_endpoints.first.split(':')[1], db: 0, logger: Rails.logger
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
